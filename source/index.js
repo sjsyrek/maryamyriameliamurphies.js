328875fe7a51d5cf50943c083bdd02ee6d37c21f
Steven Syrek <steven.syrek@gmail.com>
2016-03-12 17:02:52 -0500

    Complete documentation through Functor

--- source/index.js
+++ source/index.js
@@ -566,12 +566,12 @@ const Functor = defines(`fmap`);
 
 /**
  * Map a function over a functor, which is a type that specifies how functions may be mapped over it.
- * {@code let lst = list(1,2,3);  // [1:2:3:[]]
- *        fmap(id, lst);          // [1:2:3:[]]
+ * {@code let lst = list(1,2,3);   // [1:2:3:[]]
+ *        fmap(id, lst);           // [1:2:3:[]]
  *        let f = x => x * 11;
  *        let g = x => x * 100;
- *        $(fmap(f))(fmap(g))(l1) // [1100:2200:3300:[]]
- *        fmap($(f)(g))(l1)       // [1100:2200:3300:[]]
+ *        $(fmap(f))(fmap(g))(lst) // [1100:2200:3300:[]]
+ *        fmap($(f)(g))(lst)       // [1100:2200:3300:[]]
  * }
  * Haskell: fmap :: (a -> b) -> f a -> f b
  * @param {function()} f - The function to map.
@@ -584,20 +584,15 @@ function fmap(f, a) {
 }
 
 /**
- * Map a function over a functor, which is a type that specifies how functions may be mapped over it.
- * {@code let lst = list(1,2,3);  // [1:2:3:[]]
- *        fmap(id, lst);          // [1:2:3:[]]
- *        let f = x => x * 11;
- *        let g = x => x * 100;
- *        $(fmap(f))(fmap(g))(l1) // [1100:2200:3300:[]]
- *        fmap($(f)(g))(l1)       // [1100:2200:3300:[]]
+ * Replace all locations in a functor with the same value. Example:
+ * {@code let lst = list(1,2,3); // [1:2:3:[]]
+ *        fmapReplaceBy(5, lst)  // [5:5:5:[]]
  * }
- * Haskell: fmap :: (a -> b) -> f a -> f b
- * @param {function()} f - The function to map.
- * @param {*} - The functor to map over.
- * @return {*} - A new functor of the same type, the result of the mapping.
+ * Haskell: (<$) :: a -> f b -> f a
+ * @param {*} a - The value to inject into the functor.
+ * @param {*} b - The functor to map over.
+ * @return {*} - A new functor of the same type, with the values of the original replaced by the new value.
  */
-(<$) :: a -> f b -> f a
 function fmapReplaceBy(a, b) {
   let p = (a, b) => fmap(constant(a), b);
   return partial(p, a, b);
