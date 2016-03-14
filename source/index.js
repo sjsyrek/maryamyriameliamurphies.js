59852de2f2d43501842c912d78d7313f2298d881
Steven Syrek <steven.syrek@gmail.com>
2016-03-14 10:44:13 -0400

    Finish docs up to Ord

--- source/index.js
+++ source/index.js
@@ -441,16 +441,23 @@ function print(a) { return console.log(show(a)); }
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 // Eq
 
-// The Eq type class defines equality and inequality. Instances of Eq must provide an {@code isEq} function.
+// The `Eq` type class defines equality and inequality. Instances of `Eq` must define an `isEq` method.
 const Eq = defines(`isEq`);
 
 /**
- * Compare two objects for equality. Both objects must be instances of the Eq type class (i.e. they
- * both define an {@code isEq} static method).
+ * Compare two objects for equality. Both objects must be instances of the `Eq` type class (i.e. they
+ * both define an `isEq` static method) and must also be the same data type (or the same primitive type).
  * Haskell> (==) :: a -> a -> Bool
  * @param {*} a - Any object.
  * @param {*} b - Any object.
  * @returns {boolean} - a === b
+ * @example
+ * let lst1 = list(1,2,3);
+ * let lst2 = list(4,5,6);
+ * isEq(lst1, lst2);        // => false
+ * isEq(lst1, list(1,2,3)); // => true
+ * isEq(0, 1);              // => false
+ * isEq(0, 0);              // => true
  */
 function isEq(a, b) {
   let p = (a, b) => {
@@ -461,12 +468,19 @@ function isEq(a, b) {
 }
 
 /**
- * Compare two objects for inequality. Both objects must be instances of the Eq type class (i.e. they
- * both define an isEq static method).
+ * Compare two objects for inequality. Both objects must be instances of the `Eq` type class (i.e. they
+ * both define an `isEq` static method) and must be also be the same data type (or the same primitive type).
  * Haskell> (/=) :: a -> a -> Bool
  * @param {*} a - Any object.
  * @param {*} b - Any object.
  * @returns {boolean} - a !== b
+ * @example
+ * let lst1 = list(1,2,3);
+ * let lst2 = list(4,5,6);
+ * isNotEq(lst1, lst2);        // => true
+ * isNotEq(lst1, list(1,2,3)); // => false
+ * isNotEq(0, 1);              // => true
+ * isNotEq(0, 0);              // => false
  */
 function isNotEq(a, b) {
   let p = (a, b) => !isEq(a, b);
