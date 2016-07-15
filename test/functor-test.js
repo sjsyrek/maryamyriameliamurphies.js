f1154c907ca2a8e9e6cc9cbf089582ce80ea0e65
Steven Syrek <steven.syrek@gmail.com>
2016-07-15 16:58:29 -0400

    Add test to Functor to assert an error exception

--- test/functor-test.js
+++ test/functor-test.js
@@ -26,10 +26,10 @@ describe(`Functor type class`, function() {
   let lst2 = list(1100,2200,3300);
   let mb = just(5)
   let tup = tuple(1,2);
+  let f = x => x * 11;
+  let g = x => x * 100;
   describe(`fmap()`, function() {
     it(`should map a function over a functor and return the correct value`, function() {
-      let f = x => x * 11;
-      let g = x => x * 100;
       isEq(fmap(id, mb), mb).should.be.true;
       isEq(fmap(f, mb), just(55)).should.be.true;
       isEq(fmap(id, tup), tup).should.be.true;
@@ -38,6 +38,9 @@ describe(`Functor type class`, function() {
       isEq(fmap(f, lst1), list(11,22,33)).should.be.true;
       isEq($(fmap(f))(fmap(g))(lst1), lst2).should.be.true;
       isEq(fmap($(f)(g))(lst1), lst2).should.be.true;
+    });
+    it(`should throw an error if the second argument is not a functor`, function() {
+      fmap.bind(null, f, 0).should.throw;
     });
   });
   describe(`fmapReplaceBy()`, function() {
